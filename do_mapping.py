e089741d56d2ee102e7d390f447b1d5e0cc4bd75
Marco Ponzi <marco.ponzi@gmail.com>
2019-12-12 16:33:34 +0100

    word histogram

--- do_mapping.py
+++ do_mapping.py
@@ -8,7 +8,7 @@ from os.path import isfile, join
 import operator, math
 
 def frmt(myfloat):
-  return "{:.2f}".format(myfloat)
+  return "{:.7f}".format(myfloat)
   
   
 def top_n_sequences(string, N, max_len):
@@ -26,8 +26,23 @@ def top_n_sequences(string, N, max_len):
      res.append(srt[i][0])
   return res
   
+def word_histogram(string):
+  words=filter(None, re.split("[.|]+", string))
+  res=dict()
+  
+  # sum of histo must be 1
+  unit=1.0/float(len(words))
+  
+  for w in words:
+    if not(w in res.keys()):
+      res[w]=unit
+    else:
+      res[w]=res[w]+unit
+  return res
+  
+  
 
-def bigram_histrogram(string):
+def bigram_histogram(string):
   res=dict()
   
   # sum of histo must be 1
@@ -38,12 +53,13 @@ def bigram_histrogram(string):
       res[cur]=unit
     else:
       res[cur]=res[cur]+unit
-  return  dict(sorted(res.items(), key=operator.itemgetter(1),reverse=True))
+  #return  dict(sorted(res.items(), key=operator.itemgetter(1),reverse=True))
+  return res
   
 
 def histo_difference(h1,h2):
   tot=0
-  print set(h1.keys()).union(h2.keys())
+  #print set(h1.keys()).union(h2.keys())
   for k in set(h1.keys()).union(h2.keys()):
     v1=0
     if (k in h1.keys()):
@@ -57,24 +73,53 @@ def histo_difference(h1,h2):
   
   
 # reads whole file into a string
+# | for newline
+# . for space
 def read_file(fname):
 	data=''
 	with open(fname, 'r') as myfile:
 	    data=' '+myfile.read()
 
+	data=re.sub(r'<[^>]*>', ' ', data)
 	data=re.sub(r' *\n *', '|', data)
+	data=re.sub(r'\|\|*', '|', data)
 	data=re.sub(r'  *', '.', data)
 	return data
 
+# altering data1 to make it like data2
+def loop_replace(data1,data2):
+  # word_ can be changed to bigram_histogram
+  h1=word_histogram(data1)
+  h2=word_histogram(data2)
+  orig_diff=histo_difference(h1,h2)
+  top_n_1=top_n_sequences(data1,30,4)
+  top_n_1.remove('.')
+  top_n_1.remove('|')
+  top_n_2=top_n_sequences(data2,30,4)
+  
+  for seq1 in top_n_1:
+    for seq2 in top_n_2:
+      if (seq1!=seq2):
+        replaced=data1.replace(seq1,seq2)
+        h1=word_histogram(replaced)
+        #print data1
+        #print replaced
+        print seq1+' -> '+seq2+' '+frmt(histo_difference(h1,h2)-orig_diff)
+      else:
+        print 'EQ:'+seq1
 
 
 data1=read_file(sys.argv[1])
 data2=read_file(sys.argv[2])
 
-print data1
-h1= bigram_histrogram(data1)
-print data2
-h2= bigram_histrogram(data2)
+print data1[:1000]
+h1= word_histogram(data1)
+print data2[:1000]
+h2= word_histogram(data2)
 print histo_difference(h1,h2)
 
+#print word_histogram(data1)
+#print word_histogram(data2)
+
 print top_n_sequences(data1,500,3)
+loop_replace(data1,data2)
